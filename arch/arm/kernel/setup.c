add1c73a4337e24e63be74b9b839dca8c4bd0ee9
Linus Walleij <linus.walleij@linaro.org>
2020-10-25 23:55:16 +0100

    ARM: 9016/2: Initialize the mapping of KASan shadow memory
    This patch initializes KASan shadow region's page table and memory.
    There are two stage for KASan initializing:
    
    1. At early boot stage the whole shadow region is mapped to just
       one physical page (kasan_zero_page). It is finished by the function
       kasan_early_init which is called by __mmap_switched(arch/arm/kernel/
       head-common.S)
    
    2. After the calling of paging_init, we use kasan_zero_page as zero
       shadow for some memory that KASan does not need to track, and we
       allocate a new shadow space for the other memory that KASan need to
       track. These issues are finished by the function kasan_init which is
       call by setup_arch.
    
    When using KASan we also need to increase the THREAD_SIZE_ORDER
    from 1 to 2 as the extra calls for shadow memory uses quite a bit
    of stack.
    
    As we need to make a temporary copy of the PGD when setting up
    shadow memory we create a helpful PGD_SIZE definition for both
    LPAE and non-LPAE setups.
    
    The KASan core code unconditionally calls pud_populate() so this
    needs to be changed from BUG() to do {} while (0) when building
    with KASan enabled.
    
    After the initial development by Andre Ryabinin several modifications
    have been made to this code:
    
    Abbott Liu <liuwenliang@huawei.com>
    - Add support ARM LPAE: If LPAE is enabled, KASan shadow region's
      mapping table need be copied in the pgd_alloc() function.
    - Change kasan_pte_populate,kasan_pmd_populate,kasan_pud_populate,
      kasan_pgd_populate from .meminit.text section to .init.text section.
      Reported by Florian Fainelli <f.fainelli@gmail.com>
    
    Linus Walleij <linus.walleij@linaro.org>:
    - Drop the custom mainpulation of TTBR0 and just use
      cpu_switch_mm() to switch the pgd table.
    - Adopt to handle 4th level page tabel folding.
    - Rewrite the entire page directory and page entry initialization
      sequence to be recursive based on ARM64:s kasan_init.c.
    
    Ard Biesheuvel <ardb@kernel.org>:
    - Necessary underlying fixes.
    - Crucial bug fixes to the memory set-up code.
    
    Co-developed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Co-developed-by: Abbott Liu <liuwenliang@huawei.com>
    Co-developed-by: Ard Biesheuvel <ardb@kernel.org>
    
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: kasan-dev@googlegroups.com
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Acked-by: Mike Rapoport <rppt@linux.ibm.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Tested-by: Ard Biesheuvel <ardb@kernel.org> # QEMU/KVM/mach-virt/LPAE/8G
    Tested-by: Florian Fainelli <f.fainelli@gmail.com> # Brahma SoCs
    Tested-by: Ahmad Fatoum <a.fatoum@pengutronix.de> # i.MX6Q
    Reported-by: Russell King - ARM Linux <rmk+kernel@armlinux.org.uk>
    Reported-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Abbott Liu <liuwenliang@huawei.com>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -63,6 +63,7 @@
 #include <asm/unwind.h>
 #include <asm/memblock.h>
 #include <asm/virt.h>
+#include <asm/kasan.h>
 
 #include "atags.h"
 
@@ -1127,6 +1128,7 @@ void __init setup_arch(char **cmdline_p)
 	early_ioremap_reset();
 
 	paging_init(mdesc);
+	kasan_init();
 	request_standard_resources(mdesc);
 
 	if (mdesc->restart)
